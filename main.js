8bc432edc2c6bee03e2688e1af98b8a965252823
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-09 13:08:19 -0500

    completed

--- main.js
+++ main.js
@@ -7,53 +7,61 @@ const myCodeImmersivesObj = {
 // You won't change lines 1-3 above, but instead set and change the properties for each step below by writing NEW code underneath each comment.
 
 // Below, set the value of property `wait` to be "I object".
-
+myCodeImmersivesObj.wait = "I object";
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
-
+myCodeImmersivesObj.isAnExcellentObject = true;
 
 // Add the property `term` and set it to 1.
-
+myCodeImmersivesObj.term = 1;
 
 // Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
-
+myCodeImmersivesObj.term += 1;
 
 // Now add a new property called `currentScore` and set it to 3.
-
+myCodeImmersivesObj.currentScore = 3;
 
 // Now add a new property called `cumulativeScore` and set it to 50.
-
+myCodeImmersivesObj.cumulativeScore = 50;
 
 // Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
-
+myCodeImmersivesObj.cumulativeScore = myCodeImmersivesObj.cumulativeScore + myCodeImmersivesObj.currentScore;
 
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
-
+myCodeImmersivesObj["I am exactly this cool"] = "soooooo cool";
 
 // Part 2 - your own object.
 // Now make a new EMPTY object that represents yourself. You won't put any properties in the curly braces, but just like in part 1, add them in each step below using plain JavaScript statements.
-
+let kyle = {};
 
 // Add a `firstName` property and set it to a string.
-
+kyle.firstName = "Kyle";
 
 // Add a `lastName` property and set it to a string.
-
+kyle.lastName = "Rose";
 
 // Add a `fullName` property and set it to the name properties put together with a space string in the middle.
-
+kyle.fullName = kyle.firstName + " " + kyle.lastName;
 
 // Add an `age` property and set it to a number.
-
+kyle.age = 28;
 
 // Add an address property and set it to an empty array.
-
+kyle.address = [];
 
 // Now push three strings into that array.
-
+kyle.address.push(10811);
+kyle.address.push('Begonia');
+kyle.address.push('Way');
 
 // Part 3 - setting key-value pairs in-line.
 // Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `codeImmersivesObject` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of numbers. The important thing here is: this should all be done within the brackets of the object, not below the place where the object is declared.
-
+const myNewObject = {
+  number: 25,
+  pet: 'dog',
+  catLover: false,
+  countries: ['USA', 'Canada', 'Germany', 'Hungary', 'Greece', 'South Korea', 'China', 'Vietnam', 'Philippines'],
+  '2chainz' : 'What?'
+}
 
 // As a stretch goal, go back pre-populate the object with at least one property using a key name that CAN'T be a variable name. That would be anything with a space in it, or a dash, or starting with a number.
